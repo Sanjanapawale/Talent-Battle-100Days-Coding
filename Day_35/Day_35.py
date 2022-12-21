032c2318b750fe0e564df7864909852cc45cc489
Sanjanapawale <55655669+Sanjanapawale@users.noreply.github.com>
2022-12-21 20:38:51 +0530

    Update Day_35.py

--- Day_35/Day_35.py
+++ Day_35/Day_35.py
@@ -1,6 +1,14 @@
-string = input("Enter a String including number:")
-count = 0
-for i in string:
-    if i.isdigit():
-        count += int(i)
-print(count)
+Str1 = input('Enter a string:')
+Sum = 0
+for i in Str1:
+    if ord(i) >= 48 and ord(i) <= 57:
+        Sum = Sum + int(i)
+print('Sum is: ' + str(Sum))
+
+
+# string = input("Enter a String including number:")
+# count = 0
+# for i in string:
+#     if i.isdigit():
+#         count += int(i)
+# print(count)
