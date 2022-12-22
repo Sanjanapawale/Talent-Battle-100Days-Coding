7709e864326d092e2b7a4afd3db2e9fe78797036
Sanjanapawale <55655669+Sanjanapawale@users.noreply.github.com>
2022-12-22 19:49:45 +0530

    Update Day_36.py

--- Day_36/Day_36.py
+++ Day_36/Day_36.py
@@ -1,9 +1,13 @@
-str1 = input("Enter a string: ")
-str2 = str1.capitalize()
-last_letter = str2[-1].upper()
-str3 = ""
-for i in range(len(str2)-1):
-    str3 += str2[i]
-print(str3+last_letter, end="")
+Str1 = input('Enter a string: ')
+Str1 = Str1[0:1].upper() + Str1[1:len(Str1)-1] + Str1[len(Str1)-1:len(Str1)].upper()
+print(Str1)
+
+# str1 = input("Enter a string: ")
+# str2 = str1.capitalize()
+# last_letter = str2[-1].upper()
+# str3 = ""
+# for i in range(len(str2)-1):
+#     str3 += str2[i]
+# print(str3+last_letter, end="")
 
 
