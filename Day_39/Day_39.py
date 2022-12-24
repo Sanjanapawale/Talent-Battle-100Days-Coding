21ce83d508647d341804069f01022495156765aa
Sanjanapawale <55655669+Sanjanapawale@users.noreply.github.com>
2022-12-24 21:57:00 +0530

    Update Day_39.py

--- Day_39/Day_39.py
+++ Day_39/Day_39.py
@@ -2,8 +2,6 @@ str1, str2 = input("Enter a String: ").split()
 if len(str1) != len(str2):
     print(str1, "and", str2, "are not Anagram")
 else:
-    # li1 = list(str1)
-    # li2 = list(str2)
     if sorted(str1.lower()) == sorted(str2.lower()):
         print(str1, "and", str2, "are Anagram")
     else:
